aa8a0dc755c0413e888ce84d98a3a59b9d94bbb1
kaky <lachapelle.kaky@gmail.com>
2022-05-23 00:32:32 +0300

    modified model

--- contents/contentModel.js
+++ contents/contentModel.js
@@ -6,7 +6,7 @@ const contentSchema = new Schema({
     title: {type: String, required: true},
     text: {type: String, required: true},
     images: [{type: String, required: false}],
-    videos: [{type: String, required: false}]
+    video: {type: String, required: false}
 });
 
 const Content = mongoose.model('Content', contentSchema);
